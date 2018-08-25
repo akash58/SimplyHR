ec5c1b0e2369b1a660620ccf6581927004eb51ed
akash58 <akashvishwakarma58@gmail.com>
2018-08-25 19:40:23 +0530

    Added login page and landing page

---
+++ layouts/child.vue
@@ -0,0 +1,172 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      :mini-variant.sync="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      fixed
+      app
+      width="260"
+    >
+      <v-list>
+        <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="primary"
+    fixed
+    dark
+    app
+    :clipped-left="clipped">
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+        flat
+        solo-inverted
+        label="Search"
+        class="hidden-sm-and-down mt-2"
+        >
+      </v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="handleFullScreen()">
+        <v-icon>fullscreen</v-icon>
+      </v-btn>
+      <v-menu offset-y origin="center center" class="elelvation-1" :nudge-bottom="14" transition="scale-transition">
+        <v-btn icon flat slot="activator">
+        <v-badge color="red" overlap>
+          <span slot="badge">3</span>
+          <v-icon medium>notifications</v-icon>
+        </v-badge>
+        </v-btn>
+        <notification-list></notification-list>
+      </v-menu>
+      <v-menu offset-y origin="center center" :nudge-bottom="10" transition="scale-transition">
+        <v-btn icon large flat slot="activator">
+          <v-avatar size="30px">
+            <img src="avatar/man_4.jpg" alt="Michael Wang"/>
+          </v-avatar>
+        </v-btn>
+        <v-list class="pa-0">
+          <v-list-tile @click="" ripple="ripple">
+            <v-list-tile-action>
+              <v-icon>account_circle</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Profile</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click="" ripple="ripple">
+            <v-list-tile-action>
+              <v-icon>settings</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Settings</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <nuxt-link to="/" class="tdn black--text">
+            <v-list-tile @click="" ripple="ripple">
+              <v-list-tile-action>
+                <v-icon>fullscreen_exit</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>Logout</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </nuxt-link>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <app-fab></app-fab>
+    <v-btn small fab dark falt fixed top="top" right="right" class="setting-fab" color="primary" @click="openThemeSettings">
+      <v-icon>settings</v-icon>
+    </v-btn>
+    <v-navigation-drawer
+      class="setting-drawer"
+      temporary
+      right
+      v-model="rightDrawer"
+      hide-overlay
+      fixed
+      >
+      <theme-settings></theme-settings>
+    </v-navigation-drawer>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  import NotificationList from '~/components/NotificationList.vue'
+  import AppFab from '~/components/AppFab.vue'
+  import ThemeSettings from '~/components/ThemeSettings.vue'
+  export default {
+    components: {
+      NotificationList,
+      AppFab,
+      ThemeSettings
+    },
+    data () {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'apps', title: 'Dashboard', to: '/Dashboard' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Simply HR'
+      }
+    },
+    methods: {
+      openThemeSettings () {
+        this.$vuetify.goTo(0)
+        this.rightDrawer = (!this.rightDrawer)
+      },
+      handleFullScreen () {
+        let doc = window.document
+        let docEl = doc.documentElement
+        let requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen
+        let cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen
+        if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+          requestFullScreen.call(docEl)
+        } else {
+          cancelFullScreen.call(doc)
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .tdn {
+    text-decoration: none;
+  }
+  .setting-fab 
+    top:50%!important; 
+    right:0;
+    border-radius:0  
+  .page-wrapper
+    min-height:calc(100vh - 64px - 50px - 81px );  
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,260 +1,32 @@
 <template>
-  <div >
-    <v-container grid-list-xl fluid>
-      <v-layout row wrap>
-        <!-- mini statistic start -->
-        <v-flex lg3 sm6 xs12>
-          <mini-statistic
-            icon="group"
-            title="100+"
-            sub-title="Likes"
-            color="indigo"      
-          >
-          </mini-statistic>  
-        </v-flex>
-        <v-flex lg3 sm6 xs12>
-          <mini-statistic
-            icon="person_add"
-            title="150+"
-            sub-title="Connections"
-            color="red"      
-          >
-          </mini-statistic>           
-        </v-flex>          
-        <v-flex lg3 sm6 xs12>
-          <mini-statistic
-            icon="person"
-            title="200+"
-            sub-title="Followers"
-            color="light-blue"      
-          >
-          </mini-statistic>            
-        </v-flex>
-        <v-flex lg3 sm6 xs12>
-          <mini-statistic
-            icon="group_add"
-            title="50+"
-            sub-title="Shots"
-            color="purple"      
-          >
-          </mini-statistic>             
-        </v-flex>
-        <v-flex lg4 sm12 xs12>
-          <profile-card>
-          </profile-card>
-        </v-flex>
-        <v-flex lg4 sm12 xs12>
-          <linear-statistic 
-            title="Sales"
-            sub-title="Sales increase"
-            icon="trending_up"
-            color="success"
-            :value="15"
-          >
-          </linear-statistic>
-          <linear-statistic class="my-4"
-            title="Orders"
-            sub-title="Increase"
-            icon="trending_up"
-            color="pink"
-            :value="30"
-          >
-          </linear-statistic>          
-          <linear-statistic class="my-4"
-            title="Revenue"
-            sub-title="Revenue increase"
-            icon="trending_up"
-            color="primary"
-            :value="50"
-          >
-          </linear-statistic>          
-          <linear-statistic class="mt-4"
-            title="Cost"
-            sub-title="Cost reduce"
-            icon="trending_down"
-            color="orange"
-            :value="25"
-          >
-          </linear-statistic>          
-        </v-flex>
-        <v-flex lg4 sm12 xs12>
-          <profile-card>
-          </profile-card>
-        </v-flex>
-        <v-flex lg4 sm12 xs12 v-for="(item,index) in trending" :key="'c-trending'+index">
-          <circle-statistic
-            :title="item.subheading"
-            :sub-title="item.headline"
-            :caption="item.caption"
-            :icon="item.icon.label"
-            :color="item.linear.color"
-            :value="item.linear.value"
-          >
-          </circle-statistic>            
-        </v-flex>
-        <v-flex lg6 sm12 xs12>
-          <v-widget title="Activities" contentBg="white">
-            <div slot="widget-content">
-              <ol class="timeline timeline-activity timeline-point-sm timeline-content-right">
-                <li class="timeline-block" v-for="item in activity">
-                  <div class="timeline-point">
-                    <v-circle dot large :color="item.color"></v-circle>
-                  </div>
-                  <div class="timeline-content">
-                    <time datetime="2018" class="subheading">{{item.timeString}}</time>
-                    <div class="py-2 text--secondary" v-html="item.text"></div>
-                  </div>
-                </li>
-              </ol>              
-            </div>
-          </v-widget>          
-        </v-flex>
-        <v-flex lg12 sm12 xs12>
-          <Table></Table>
-        </v-flex>
-      </v-layout>
-    </v-container>
+  <div>
+    <v-system-bar status color="primary" class="white--text px-5">
+      <div>BOOM HR</div>
+      <v-spacer></v-spacer>
+      <nuxt-link to="/Login" class="tdn white--text">
+        <div>SIGN IN</div>
+      </nuxt-link>
+    </v-system-bar>
+    <v-layout class="px-5 py-3 grey lighten-5">BOOM HR LOGO</v-layout>
+    <div class="bg">ASasASSas</div>
   </div>
 </template>
-<script>
-  import MiniStatistic from '~/components/statistic/MiniStatistic.vue'
-  import ProfileCard from '~/components/statistic/card/ProfileCard.vue'
-  import LinearStatistic from '~/components/statistic/LinearStatistic.vue'
-  import CircleStatistic from '~/components/statistic/CircleStatistic.vue'
-  import VCircle from '~/components/circle/VCircle'
-  import VWidget from '~/components/VWidget'
-  import Table from '~/components/Table'
-  export default {
-    components: {
-      MiniStatistic,
-      ProfileCard,
-      LinearStatistic,
-      CircleStatistic,
-      VCircle,
-      VWidget,
-      Table
-    },
-    data: () => ({
-      trending: [
-        {
-          subheading: 'Email',
-          headline: '15+',
-          caption: 'email opens',
-          percent: 15,
-          icon: {
-            label: 'email',
-            color: 'info'
-          },
-          linear: {
-            value: 15,
-            color: 'info'
-          }
-        },
-        {
-          subheading: 'Tasks',
-          headline: '90%',
-          caption: 'tasks completed.',
-          percent: 90,
-          icon: {
-            label: 'list',
-            color: 'primary'
-          },
-          linear: {
-            value: 90,
-            color: 'success'
-          }
-        },
-        {
-          subheading: 'Issues',
-          headline: '100%',
-          caption: 'issues fixed.',
-          percent: 100,
-          icon: {
-            label: 'bug_report',
-            color: 'primary'
-          },
-          linear: {
-            value: 100,
-            color: 'error'
-          }
-        }
-      ],
-      activity: [
-        {
-          avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/ludwiczakpawel/128.jpg',
-          timeString: 'Just now',
-          color: 'primary',
-          text: 'Michael finished  one task just now.'
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/suprb/128.jpg',
-          timeString: '30 min ago',
-          color: 'teal',
-          text: 'Jim created a new  task.'
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/suprb/128.jpg',
-          timeString: '1 hour ago',
-          color: 'indigo',
-          text: 'Li completed the PSD to html convert.'
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/suprb/128.jpg',
-          timeString: '3 hour ago',
-          color: 'pink',
-          text: 'Michael upload a new pic.'
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/suprb/128.jpg',
-          timeString: '10 min ago',
-          color: 'cyan',
-          text: 'Li assigned a a task to Michael'
-        }
-      ]
-    })
+
+<style lang="stylus" scoped>
+  .bg {
+    /* The image used */
+    background-image: url("/creative_process/11.jpeg");
+
+    /* Full height */
+    height: 500px; 
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
   }
-</script>
-<style scoped>
-  timeline {
-    position: relative;
-    list-style: none;
-    padding-left: 0;
-    z-index: 1;
-}
-ol {
-    display: block;
-    list-style-type: decimal;
-    -webkit-margin-before: 1em;
-    -webkit-margin-after: 1em;
-    -webkit-margin-start: 0px;
-    -webkit-margin-end: 0px;
-    -webkit-padding-start: 40px;
-}
-.timeline-block {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-}
-.timeline-point-sm .timeline-point {
-    width: 64px;
-}
-.timeline-point {
-    position: relative;
-    -ms-flex-negative: 0;
-    flex-shrink: 0;
-    -webkit-box-flex: 0;
-    -ms-flex-positive: 0;
-    flex-grow: 0;
-    -webkit-box-ordinal-group: 3;
-    -ms-flex-order: 2;
-    order: 2;
-    width: 96px;
-    padding-bottom: 30px;
-    text-align: center;
-}
-.timeline.timeline-content-right .timeline-block .timeline-content {
-    -webkit-box-ordinal-group: 4;
-    -ms-flex-order: 3;
-    order: 3;
-}
+  .tdn {
+    text-decoration: none;
+  }
 </style>
+  
